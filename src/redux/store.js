b7fe15721d56de0f290f7394da29f9d2742287e2
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-02-27 10:43:41 -0700

    added redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import promiseMiddleware from 'redux-promise-middleware'
 import reducer from './reducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-export default createStore(reducer, applyMiddleware(promiseMiddleware))
+export default createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(promiseMiddleware))
+)
